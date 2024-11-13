821533554990a27af471639e39a464f8be3ec2c7
J-Stanton <jameskstanton123@gmail.com>
2024-11-13 22:09:42 +0200

    Added link to handicap form page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 import './Home.css';
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Home(){
+    const navigate = useNavigate();
+
+    const handleGoToHandicapForm = ()=>{
+        navigate("/handicap-form")
+    }
     return(
         <div className="home">
             <header className="home-header">
                 <h1>Early Birds Golf Tracker</h1>
             </header>
             <div className="home-buttons">
-                <button className="home-button">Add Handicap Scores</button>
+                <button className="home-button" onClick={handleGoToHandicapForm}>Add Handicap Scores</button>
                 <button className="home-button">View Handicaps</button>
                 <button className="home-button">Add Competition Scores</button>
                 <button className="home-button">View Competition Scores</button>
